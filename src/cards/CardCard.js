df885f5586e77c9fcbb49e4d6348b57b362192f2
danfyfe <fyfedaniel@gmail.com>
2019-11-08 17:42:47 -0500

    basic drag and drop

--- src/cards/CardCard.js
+++ src/cards/CardCard.js
@@ -2,11 +2,15 @@ import React from 'react'
 
 const CardCard = props => {
 
-    const { cardObj, handleDragStart, cardId } = props 
+    const { cardObj, handleDragStart, handleDragEnd, handleDragOver, cardId, index } = props 
     const { title, content } = cardObj 
 
     return(
-        <div id={cardId} draggable className='card' onDragStart={ () => handleDragStart(cardObj, cardId)}>
+        <div id={cardId} draggable className='card' 
+        onDragOver={e => handleDragOver(index)}
+        onDragStart={ e => handleDragStart(index, e)}
+        onDragEnd={e => handleDragEnd(index, e)}
+        >
             <span>{title}</span>
             <p>{content}</p>
         </div>
--- src/lists/CardList.js
+++ src/lists/CardList.js
@@ -1,24 +1,62 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import CardCard from '../cards/CardCard'
 import { CardContext } from '../App'
 
 
 const CardList = props => {
 
-    const cardObjects = useContext(CardContext)
+    // const cardObjects = useContext(CardContext)
+    
+    const origCardObjects = [
+        { id: 1, title: 'Buttz!', content: 'Blah blah blah!' },
+        { id:2, title: 'Not Buttz!', content: 'Whatever!' },
+        { id:3, title: 'Also not Buttz!', content: 'You dont know!' }
+    ]
 
-    const handleDragStart = (cardObj, cardId) => {
-        const card = document.getElementById(cardId)
-        card.style.backgroundcolor = 'red'
-        console.log(card)
+    const [ cardObjects, setCardObjects ] = useState(origCardObjects)
+    const [ draggedItem, setDraggedItem ] = useState()
+
+    const handleDragStart = (index, e) => {
+        // e.target.className = 'card-drag'
+        let draggedCard = cardObjects[index]
+
+        setDraggedItem(draggedCard)
+
+        e.dataTransfer.effectAllowed = 'move'
+        e.dataTransfer.setData('text/html', e.target)
+        e.dataTransfer.setDragImage(e.target, 20, 20)
+    }
+
+    const handleDragEnd = (index, e) => {
+        // e.target.className = 'card'
+
+   
+    }
+
+    const handleDragOver = index => {
+        const draggedOverCard = cardObjects[index]
+
+        if( draggedOverCard === draggedItem) {
+            return;
+        }
+
+        let items = cardObjects.filter( item => item !== draggedItem)
+
+        items.splice(index, 0, draggedItem)
+
+        setCardObjects(items)
     }
 
     const renderCards = cardObjs => {
-        return cardObjs.map( cardObj => {
-            return <CardCard key={cardObj.id} cardId={cardObj.id} cardObj={cardObj} handleDragStart={handleDragStart}/>
+        return cardObjs.map( (cardObj, index) => {
+            return <CardCard key={cardObj.id} 
+            cardId={cardObj.id} cardObj={cardObj} 
+            handleDragStart={handleDragStart} 
+            handleDragEnd={handleDragEnd}
+            handleDragOver={handleDragOver}
+            index={index}/>
         })
     }
-
 
 
     return(
